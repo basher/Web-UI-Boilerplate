c0dd4d2d4792af26934171d7183517a6437f983c
basher <basher@madasafish.com>
2023-05-04 11:00:24 +0100

    add video iframe lazyload fallback

--- ui/src/javascript/modules/video-player.ts
+++ ui/src/javascript/modules/video-player.ts
@@ -3,12 +3,14 @@ export default class VideoPlayer {
     private btnPlay: HTMLButtonElement | null;
     private mediaContainer: HTMLElement | null;
     private iframe: HTMLIFrameElement | null;
+    private videoLink: HTMLAnchorElement | null;
 
     constructor(player: Element) {
         this.player = player;
         this.btnPlay = this.player.querySelector('[data-button="modal-open"]');
         this.mediaContainer = this.player.querySelector('.responsive-media');
-        this.iframe = this.player.querySelector('iframe');
+        this.iframe = this.player.querySelector('.responsive-media__item');
+        this.videoLink = this.player.querySelector('.video-player__link');
 
         this.init();
     }
@@ -25,13 +27,29 @@ export default class VideoPlayer {
     }
 
     private init(): void {
+        this.lazyload();
         this.btnPlay?.addEventListener('click', (e: MouseEvent) =>
             this.handlePlay(e),
         );
     }
 
+    private lazyload(): void {
+        // Browser does not support native lazyload for iframes.
+        if (
+            'loading' in HTMLIFrameElement.prototype === false &&
+            this.videoLink?.href
+        ) {
+            this.iframe?.setAttribute('data-src', this.videoLink.href);
+            this.iframe?.setAttribute('src', '');
+        }
+    }
+
     private handlePlay(e: MouseEvent): void {
         e.preventDefault();
+
+        // Reset iframe 'src' back to original value.
+        this.iframe?.dataset.src &&
+            this.iframe?.setAttribute('src', this.iframe?.dataset.src);
 
         // Adjust aspect ratio based on video's natural dimensions.
         if (this.mediaContainer && this.iframe?.height && this.iframe.width) {
