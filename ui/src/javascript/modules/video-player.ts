52e67b9eac944ca4d9a4b4f9123292c5f934541b
basher <basher@madasafish.com>
2023-05-04 09:18:27 +0100

    fix video aspect ratio

--- ui/src/javascript/modules/video-player.ts
+++ ui/src/javascript/modules/video-player.ts
@@ -1,10 +1,14 @@
 export default class VideoPlayer {
     private player: Element;
     private btnPlay: HTMLButtonElement | null;
+    private mediaContainer: HTMLElement | null;
+    private iframe: HTMLIFrameElement | null;
 
     constructor(player: Element) {
         this.player = player;
-        this.btnPlay = player.querySelector('[data-button="modal-open"]');
+        this.btnPlay = this.player.querySelector('[data-button="modal-open"]');
+        this.mediaContainer = this.player.querySelector('.responsive-media');
+        this.iframe = this.player.querySelector('iframe');
 
         this.init();
     }
@@ -14,11 +18,10 @@ export default class VideoPlayer {
             '[data-module="video-player"]',
         );
 
-        players &&
-            players.forEach((player) => {
-                const instance = new VideoPlayer(player);
-                return instance;
-            });
+        players?.forEach((player) => {
+            const instance = new VideoPlayer(player);
+            return instance;
+        });
     }
 
     private init(): void {
@@ -29,5 +32,16 @@ export default class VideoPlayer {
 
     private handlePlay(e: MouseEvent): void {
         e.preventDefault();
+
+        // Adjust aspect ratio based on video's natural dimensions.
+        if (this.mediaContainer && this.iframe?.height && this.iframe.width) {
+            const aspectRatio =
+                parseInt(this.iframe.width, 10) /
+                parseInt(this.iframe.height, 10);
+            this.mediaContainer?.setAttribute(
+                'style',
+                `--mediaAspectRatio: ${aspectRatio}`,
+            );
+        }
     }
 }
