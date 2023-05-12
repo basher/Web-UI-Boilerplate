161ede853103f519690ce9a51182856411135962
basher <basher@madasafish.com>
2023-05-12 13:53:33 +0100

    add toggle variant

--- ui/stories/5. Components/Toggle/Toggle.stories.js
+++ ui/stories/5. Components/Toggle/Toggle.stories.js
@@ -15,14 +15,30 @@ export default {
 
 export const Toggle = () => `
 <button
-    class="button button--text toggle"
+    class="button toggle"
     type="button"
     role="switch"
     aria-checked="false"
     aria-label="toggle / switch"
 >
-    <span class="toggle--on" aria-hidden="true">on</span>
-    <span class="toggle--off" aria-hidden="true">off</span>
+    <span class="toggle__on" aria-hidden="true">on</span>
+    <span class="toggle__off" aria-hidden="true">off</span>
 </button>
 `;
-Toggle.storyName = 'Toggle button (or Switch)';
+Toggle.storyName = 'Toggle button (or switch)';
+
+export const ToggleWithLabel = () => `
+<button
+    class="button toggle toggle--has-label"
+    type="button"
+    role="switch"
+    aria-checked="false"
+>
+    <span class="toggle__label">toggle / switch label</span>
+    <span class="toggle__indicator">
+        <span class="toggle__on" aria-hidden="true">on</span>
+        <span class="toggle__off" aria-hidden="true">off</span>
+    </span>
+</button>
+`;
+ToggleWithLabel.storyName = 'Toggle with visible label';
