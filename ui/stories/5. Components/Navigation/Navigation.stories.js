77928bed2a73938e3c01030b7469f4efb05e8b37
basher <basher@madasafish.com>
2023-05-23 13:20:20 +0100

    scaffold nav components

--- ui/stories/5. Components/Navigation/Navigation.stories.js
+++ ui/stories/5. Components/Navigation/Navigation.stories.js
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-// import docs from './Navigation.mdx'
+import docs from './Navigation.mdx'
 
 export default {
     title: 'Components/Navigation',
     parameters: {
-        // docs: {
-        //     page: docs,
-        // },
+        docs: {
+            page: docs,
+        },
         status: {
             type: 'beta',
         },
@@ -14,5 +14,42 @@ export default {
 };
 
 export const Navigation = () => `
-TODO...
+<nav class="nav" aria-label="Primary">
+    <ul class="nav__list">
+        <li class="nav__item">
+            <a class="nav__link" href="#">Nav link</a>
+        </li>
+        <li class="nav__item">
+            <a class="nav__link" href="#" aria-current="page">Current page</a>
+        </li>
+        <li class="nav__item">
+            <a class="nav__link" href="#">Nav link</a>
+        </li>
+        <li class="nav__item">
+            <a class="nav__link" href="#">Nav link</a>
+        </li>
+    </ul>
+</nav>
 `;
+Navigation.storyName = 'Simple navigation';
+
+export const HamburgerNavigation = () => `
+TODO: hamburger...
+`;
+HamburgerNavigation.storyName = 'Hamburger navigation';
+
+export const DisclosureNavigation = () => `
+TODO: disclosure...
+`;
+DisclosureNavigation.storyName = 'Disclosure navigation';
+
+export const HybridDisclosureNavigation = () => `
+TODO: hybrid disclosure...
+`;
+HybridDisclosureNavigation.storyName =
+    'Hybrid disclosure navigation (with top-level links)';
+
+export const MegaNavigation = () => `
+TODO: mega-dropdown...
+`;
+MegaNavigation.storyName = 'Mega-dropdown navigation';
