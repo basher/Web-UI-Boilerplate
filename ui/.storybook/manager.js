55972dcff6e37a06c579a68843507341f6e9b6d3
basher <basher@madasafish.com>
2023-06-29 09:51:42 +0000

    Deploying to master from @ basher/Web-UI-Boilerplate@170e6cf0150e900cad34420e9ebcb24e03c47907 🚀

--- ui/.storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons'
-import WebUIBoilerplate from "./theme"
-
-addons.setConfig({
-    theme: WebUIBoilerplate,
-})
--- ui/.storybook/preview.js
+++
@@ -1,7 +0,0 @@
-export const parameters = {
-    options: {
-      storySort: {
-        order: ['WebUI Storybook', 'Design System', 'Layout','Forms', 'Components', 'Utilities', 'Pages'],
-      },
-    },
-  }
