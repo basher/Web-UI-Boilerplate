e146d46866f9d95d6f7b15e85376ba9ae233ad78
basher <basher@madasafish.com>
2023-07-03 09:52:05 +0100

    add form stories no args

---
+++ ui/stories/4. Forms/FormNoArgs.stories.js
@@ -0,0 +1,277 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import docs from './Form.mdx';
+
+export default {
+    title: 'Forms/Form',
+    parameters: {
+        docs: {
+            page: docs,
+        },
+        status: {
+            type: 'stable',
+        },
+    },
+};
+
+export const FormNoArgsServerValidation = () => `
+<div class="message message--error" role="alert">
+    <div class="stack">
+        <h2>Form errors</h2>
+        <p>
+            Optional error summary.<br/>
+            For example, rendered in the HTML on server postback.
+        </p>
+        <ul>
+            <li>
+                <a href="#input-tel">Error text... tel</a>
+            </li>
+            <li>
+                <a href="#select">Error text... select</a>
+            </li>
+            <li>
+                <a href="#checkbox-1">Error text... checkbox</a>
+            </li>
+            <li>
+                <a href="#radio-1">Error text... radio</a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<form class="form" action="#">
+    <div class="form__field">
+        <label for="input-text1" class="label">
+            Text input 1 label
+            <span aria-hidden="true">(required)</span>
+        </label>
+        <input
+            type="text"
+            id="input-text1"
+            class="input"
+            placeholder="placeholder"
+            value="example of valid field - e.g. on form postback"
+            required
+        />
+    </div>
+    <div class="form__field form__field--has-error">
+        <label for="input-tel" class="label">
+            Telephone input label
+            <span aria-hidden="true">(required)</span>
+        </label>
+        <input
+            type="tel"
+            id="input-tel"
+            class="input"
+            pattern="^[0-9-+\s()]*$"
+            placeholder="tel number, can include +-() and spaces"
+            title="tel number, can include +-() and spaces"
+            autocomplete="tel"
+            required
+            aria-invalid="true"
+            aria-describedby="input-tel-error"
+        />
+        <span class="form__error" id="input-tel-error">
+            Error text...
+        </span>
+    </div>
+    <div class="form__field form__field--has-error">
+        <label for="select" class="label">
+            Select label
+            <span aria-hidden="true">(required)</span>
+        </label>
+        <select
+            class="select"
+            id="select"
+            required
+            aria-invalid="true"
+            aria-describedby="input-tel-error"
+        >
+            <option value="">choose...</option>
+            <option value="1">option 1</option>
+            <option value="2">option 2</option>
+            <option value="3">option 3</option>
+        </select>
+        <span class="form__error" id="select-error">
+            Error text...
+        </span>
+    </div>
+    <div class="form__field form__field--has-error">
+        <div class="checkbox">
+            <input
+                type="checkbox"
+                id="checkbox-1"
+                class="checkbox__input"
+                required
+                aria-invalid="true"
+                aria-describedby="checkbox-1-error"
+            />
+            <label for="checkbox-1" class="checkbox__label">
+                Checkbox label
+                <span aria-hidden="true">(required)</span>
+            </label>
+        </div>
+        <span class="form__error" id="checkbox-1-error">
+            Error text...
+        </span>
+    </div>
+    <div class="form__field form__field--has-error">
+        <fieldset class="fieldset">
+            <legend class="legend">
+                Radio legend
+                <span aria-hidden="true">(required)</span>
+            </legend>
+            <div class="radio">
+                <input
+                    type="radio"
+                    id="radio-1"
+                    class="radio__input"
+                    name="radio_group"
+                    required
+                    aria-invalid="true"
+                    aria-describedby="radio_group-error"
+                />
+                <label for="radio-1" class="radio__label">Radio 1 label</label>
+            </div>
+            <div class="radio">
+                <input
+                    type="radio"
+                    id="radio-2"
+                    class="radio__input"
+                    name="radio_group"
+                    required
+                    aria-invalid="true"
+                    aria-describedby="radio_group-error"
+                />
+                <label for="radio-2" class="radio__label">Radio 2 label</label>
+            </div>
+        </fieldset>
+        <span class="form__error" id="radio_group-error">
+            Error text...
+        </span>
+    </div>
+
+    <div class="button-group">
+        <button
+            type="submit"
+            class="button button--text button--positive"
+        >
+            Submit
+        </button>
+        <button
+            type="reset"
+            class="button button--text"
+        >
+            Reset
+        </button>
+    </div>
+</form>
+`;
+FormNoArgsServerValidation.storyName = 'Form (with server-side validation)';
+
+export const FormNoArgsJSValidation = () => `
+<form class="form" action="#" data-module="form-validate">
+    <div class="form__field">
+        <label for="input-text1" class="label">
+            Text input 1 label
+            <span aria-hidden="true">(required)</span>
+        </label>
+        <input
+            type="text"
+            id="input-text1"
+            class="input"
+            placeholder="placeholder"
+            required
+        />
+    </div>
+    <div class="form__field">
+        <label for="input-tel" class="label">
+            Telephone input label
+            <span aria-hidden="true">(required)</span>
+        </label>
+        <input
+            type="tel"
+            id="input-tel"
+            class="input"
+            pattern="^[0-9-+\s()]*$"
+            placeholder="tel number, can include +-() and spaces"
+            title="tel number, can include +-() and spaces"
+            autocomplete="tel"
+            required
+        />
+    </div>
+    <div class="form__field">
+        <label for="select" class="label">
+            Select label
+            <span aria-hidden="true">(required)</span>
+        </label>
+        <select
+            class="select"
+            id="select"
+            required
+        >
+            <option value="">choose...</option>
+            <option value="1">option 1</option>
+            <option value="2">option 2</option>
+            <option value="3">option 3</option>
+        </select>
+    </div>
+    <div class="form__field">
+        <div class="checkbox">
+            <input
+                type="checkbox"
+                id="checkbox-1"
+                class="checkbox__input"
+                required
+            />
+            <label for="checkbox-1" class="checkbox__label">
+                Checkbox label
+                <span aria-hidden="true">(required)</span>
+            </label>
+        </div>
+    </div>
+    <div class="form__field">
+        <fieldset class="fieldset">
+            <legend class="legend">
+                Radio legend
+                <span aria-hidden="true">(required)</span>
+            </legend>
+            <div class="radio">
+                <input
+                    type="radio"
+                    id="radio-1"
+                    class="radio__input"
+                    name="radio_group"
+                    required
+                />
+                <label for="radio-1" class="radio__label">Radio 1 label</label>
+            </div>
+            <div class="radio">
+                <input
+                    type="radio"
+                    id="radio-2"
+                    class="radio__input"
+                    name="radio_group"
+                    required
+                />
+                <label for="radio-2" class="radio__label">Radio 2 label</label>
+            </div>
+        </fieldset>
+    </div>
+
+    <div class="button-group">
+        <button
+            type="submit"
+            class="button button--text button--positive"
+        >
+            Submit
+        </button>
+        <button
+            type="reset"
+            class="button button--text"
+        >
+            Reset
+        </button>
+    </div>
+</form>
+`;
+FormNoArgsJSValidation.storyName = 'Form (with JS validation)';
