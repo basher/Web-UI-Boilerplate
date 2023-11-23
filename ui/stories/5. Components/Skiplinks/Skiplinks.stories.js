620c36e8a84e35451c0ea6eeffef0f692b17156a
basher <basher@madasafish.com>
2023-11-23 10:59:04 +0000

    skiplink a11y fix

--- ui/stories/5. Components/Skiplinks/Skiplinks.stories.js
+++ ui/stories/5. Components/Skiplinks/Skiplinks.stories.js
@@ -20,4 +20,10 @@ export const Skiplinks = () => `
     <a class="skiplinks__link" href="#main">Skip to main content</a>
     <a class="skiplinks__link" href="#footer">Skip to footer</a>
 </nav>
+
+<p>Some content preceding skiplink target - Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, incidunt non numquam deserunt dolorem, perspiciatis error asperiores fuga.</p>
+
+<main id="main" tabindex="-1">
+    <p>The <code>main</code> landmark is a skiplink target. It has <code>tabindex="-1"</code> to ensure it can receive focus. See the docs for more info.</p>
+</main>
 `;
