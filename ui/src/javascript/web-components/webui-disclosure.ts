af791d64c8dc4770e179973ab3c34a0f091d1bc4
basher <basher@madasafish.com>
2024-04-02 16:25:43 +0100

    refactor webui-disclosure to nt use external dependency

--- ui/src/javascript/web-components/webui-disclosure.ts
+++ ui/src/javascript/web-components/webui-disclosure.ts
@@ -1,4 +1,4 @@
-import { disclosure } from '../utils/disclosure';
+import { randomString } from '../utils/random-string';
 
 export default class WebUIDisclosure extends HTMLElement {
     private trigger: HTMLButtonElement | null;
@@ -16,31 +16,80 @@ export default class WebUIDisclosure extends HTMLElement {
 
         this.init();
 
-        // TODO:
-        // 1. All events are handled by the external 'disclosure()' dependency.
-        // 2. Refactor to handle events here. For now, just console.log().
         this.trigger?.addEventListener('click', this);
     }
 
     private init(): void {
         if (!this.trigger || !this.content) return;
 
-        // TODO: This code might become redundant (see 2 above).
-        const button = this.trigger;
-        const content = this.content;
-        const bindEscapeKey = this.bindEscapeKey;
-        const bindClickOutside = this.bindClickOutside;
-
-        disclosure({
-            button,
-            content,
-            bindEscapeKey,
-            bindClickOutside,
-        });
+        this.a11ySetup();
     }
 
+    private a11ySetup(): void {
+        this.trigger?.removeAttribute('hidden');
+        this.trigger?.setAttribute('aria-expanded', 'false');
+        this.content?.setAttribute('hidden', '');
+
+        // Auto-generate unique 'id' and 'aria-controls' attributes, using button 'parentElement' className or nodeName as a sensible prefix.
+        if (this.trigger?.parentElement) {
+            const unique = randomString(
+                this.trigger.parentElement.classList[0] ||
+                    this.trigger.parentElement.nodeName.toLowerCase(),
+            );
+            this.content?.setAttribute('id', unique);
+            this.trigger.setAttribute('aria-controls', unique);
+        }
+    }
+
+    private hideContent(): void {
+        // Set keyboard focus to the trigger button.
+        if (this.trigger?.getAttribute('aria-expanded') === 'true') {
+            this.trigger?.focus();
+            this.trigger?.setAttribute('aria-expanded', 'false');
+        }
+        this.content?.setAttribute('hidden', '');
+    }
+
+    private handleGlobalClick(e: MouseEvent): void {
+        if (this.bindClickOutside) {
+            const target = e.target as HTMLElement;
+            const insideButton = this.trigger?.contains(target);
+            const insideContent = this.content?.contains(target);
+
+            if (!insideButton && !insideContent) {
+                this.hideContent();
+            }
+        }
+    }
+
+    private handleGlobalKeyup(e: KeyboardEvent): void {
+        if (this.bindEscapeKey && e.code === 'Escape') {
+            this.hideContent();
+        }
+    }
+
+    // Handle events from constructor().
     handleEvent(e: MouseEvent) {
-        // TODO: See 2 above.
-        console.log('handleEvent', e);
+        const target = e.currentTarget as HTMLElement;
+        const isExpanded =
+            target?.getAttribute('aria-expanded') === 'true' || false;
+
+        target?.setAttribute('aria-expanded', Boolean(!isExpanded).toString());
+        this.content?.toggleAttribute('hidden');
+    }
+
+    // Add/remove other (global) event listeners which are not part of this component.
+    connectedCallback() {
+        window.addEventListener('click', (e: MouseEvent) =>
+            this.handleGlobalClick(e),
+        );
+        window.addEventListener('keyup', (e: KeyboardEvent) =>
+            this.handleGlobalKeyup(e),
+        );
+    }
+
+    disconnectedCallback() {
+        window.removeEventListener('click', this.handleGlobalClick);
+        window.removeEventListener('keyup', this.handleGlobalKeyup);
     }
 }
