e58faaa0b62e21d7c16a4eef41b076dad5fe946f
basher <basher@madasafish.com>
2025-09-25 10:12:53 +0100

    simplify pagination BEM classes

--- ui/stories/5. Components/Pagination/Pagination.js
+++ ui/stories/5. Components/Pagination/Pagination.js
@@ -1,29 +1,29 @@
 export const PaginationHtml = () => `
 <nav class="pagination" aria-label="Pagination">
     <ul class="pagination__list flex">
-        <li class="pagination__item">
+        <li>
             <a class="button button--text button--small" href="#" aria-disabled="true" tabindex="-1">Previous</a>
         </li>
-        <li class="pagination__item">
+        <li>
             <a class="button button--text button--small" href="#" aria-current="page">1</a>
         </li>
-        <li class="pagination__item">
+        <li>
             <a class="button button--text button--small" href="#">2</a>
         </li>
-        <li class="pagination__item">
+        <li>
             <a class="button button--text button--small" href="#">3</a>
         </li>
-        <li class="pagination__item">
+        <li>
             <a class="button button--text button--small" href="#">4</a>
         </li>
-        <li class="pagination__item">
+        <li>
             <a class="button button--text button--small" href="#">5</a>
         </li>
-        <li class="pagination__item">&hellip;</li>
-        <li class="pagination__item">
+        <li>&hellip;</li>
+        <li>
             <a class="button button--text button--small" href="#">10</a>
         </li>
-        <li class="pagination__item">
+        <li>
             <a class="button button--text button--small" href="#">Next</a>
         </li>
     </ul>
