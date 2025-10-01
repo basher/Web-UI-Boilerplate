c84b3e08b54106c8db31dcfaaea862d0cad349f7
basher <basher@madasafish.com>
2025-10-01 12:00:43 +0100

    use async/await for polyfills dynamic import

--- ui/src/javascript/index.ts
+++ ui/src/javascript/index.ts
@@ -6,11 +6,16 @@ import '../../src/images/sprite.svg';
 import { browserSupportsAllFeatures } from './config/browser-supports-features';
 import { uiInit } from './ui-init';
 
-if (browserSupportsAllFeatures()) {
+if (!browserSupportsAllFeatures()) {
     uiInit();
 } else {
     // Dynamic import polyfills, then instantiate UI modules.
-    import('./utils/polyfills')
-        .then(() => uiInit())
-        .catch((e) => console.error(e));
+    (async (): Promise<void> => {
+        try {
+            await import('./utils/polyfills');
+            uiInit();
+        } catch (e) {
+            console.error(e);
+        }
+    })();
 }
