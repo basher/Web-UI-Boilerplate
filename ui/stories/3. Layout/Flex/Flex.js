501604b66e9e0391ee15dcbd980565ddeef51502
basher <basher@madasafish.com>
2025-10-07 09:34:34 +0100

    simplify flex css

--- ui/stories/3. Layout/Flex/Flex.js
+++ ui/stories/3. Layout/Flex/Flex.js
@@ -1,6 +1,6 @@
 const style = `
 <style>
-    .flex > * {
+    [class*='flex'] > * {
         background: hsl(51, 100%, 45%, 15%);
         border: 1px dashed hsl(51, 100%, 45%);
         padding: 1rem;
@@ -10,12 +10,12 @@ const style = `
 
 export const FlexHtml = (args) => `
 ${style}
-<div class="flex
-    ${args.growAllFlexItems === true ? 'flex--grow-all' : ''}
-    ${args.direction === undefined ? '' : `flex--${args.direction}`}
-    ${args.gap === undefined ? '' : `flex--${args.gap}`}
-    ${args.alignItems === undefined ? '' : `flex--${args.alignItems}`}
-    ${args.justifyContent === undefined ? '' : `flex--${args.justifyContent}`}
+<div class="flex\
+ ${args.growAllFlexItems === true ? 'flex--grow-all' : ''}\
+ ${args.direction === undefined ? '' : `flex--${args.direction}`}\
+ ${args.gap === undefined ? '' : `flex--${args.gap}`}\
+ ${args.alignItems === undefined ? '' : `flex--${args.alignItems}`}\
+ ${args.justifyContent === undefined ? '' : `flex--${args.justifyContent}`}\
 ">
     <div>Flex item 1 - lots more content<br><br>
     to demonstrate (default) equal height columns</div>
