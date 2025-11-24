3405b0ba9b7f2dafac5bc0c8c2e9a37751aaef23
basher <basher@madasafish.com>
2025-11-24 10:53:25 +0000

    add appropriate heading to <webui-notify>

--- ui/stories/6. Web Components Or Custom Elements/WebUI Notify/WebUINotify.js
+++ ui/stories/6. Web Components Or Custom Elements/WebUI Notify/WebUINotify.js
@@ -4,6 +4,9 @@ export const WebUINotifyHtml = (args) => `
     ${args.notificationType === undefined ? '' : `data-${args.notificationType}`}
 >
     <div class="stack">
+        ${args.notificationType === 'success' ? '<h2>Success</h2>' : ''}
+        ${args.notificationType === 'warning' ? '<h2>Warning</h2>' : ''}
+        ${args.notificationType === 'error' ? '<h2>Error</h2>' : ''}
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur?</p>
     </div>
