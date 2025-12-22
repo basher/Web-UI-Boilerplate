7cbc1f1161d63f2cdf69bd5198775bd839387d58
basher <basher@madasafish.com>
2025-12-22 10:58:04 +0000

    replace media query in tabs with container query

--- ui/stories/6. Web Components Or Custom Elements/WebUI Tabs/WebUITabs.js
+++ ui/stories/6. Web Components Or Custom Elements/WebUI Tabs/WebUITabs.js
@@ -34,36 +34,38 @@ export const WebUITabsHtml = () => /*html*/ `
 `;
 
 export const WebUIVerticalTabsHtml = () => /*html*/ `
-<webui-tabs data-vertical>
-    <div data-tablist>
-        <a href="#tab1vertical" data-tab>
-            Tab 1
-        </a>
-        <a href="#tab2vertical" data-tab>
-            Tab 2
-        </a>
-        <a href="#tab3vertical" data-tab>
-            Tab 3
-        </a>
-        <a href="#tab4vertical" data-tab>
-            Tab 4 with a very long title to test overflow
-        </a>
-    </div>
-    <div id="tab1vertical" data-tabpanel>
-        <p>1 - Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        <br>
-        <b><a href="#">Use the <kbd>tab</kbd> key to access this link inside the tab content</a></b>.
-        <br>
-        Voluptatum maxime amet omnis numquam voluptatem exercitationem nemo deleniti dolorem perferendis vitae? Qui molestias inventore illum ad ullam eum unde omnis temporibus.</p>
-    </div>
-    <div id="tab2vertical" data-tabpanel>
-        <p>2 - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum maxime amet omnis numquam voluptatem exercitationem nemo deleniti dolorem perferendis vitae? Qui molestias inventore illum ad ullam eum unde omnis temporibus.</p>
-    </div>
-    <div id="tab3vertical" data-tabpanel>
-        <p>3 - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum maxime amet omnis numquam voluptatem exercitationem nemo deleniti dolorem perferendis vitae? Qui molestias inventore illum ad ullam eum unde omnis temporibus.</p>
-    </div>
-    <div id="tab4vertical" data-tabpanel>
-        <p>4 - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum maxime amet omnis numquam voluptatem exercitationem nemo deleniti dolorem perferendis vitae? Qui molestias inventore illum ad ullam eum unde omnis temporibus.</p>
-    </div>
-</webui-tabs>
+<div class="tabs-container">
+    <webui-tabs data-vertical>
+        <div data-tablist>
+            <a href="#tab1vertical" data-tab>
+                Tab 1
+            </a>
+            <a href="#tab2vertical" data-tab>
+                Tab 2
+            </a>
+            <a href="#tab3vertical" data-tab>
+                Tab 3
+            </a>
+            <a href="#tab4vertical" data-tab>
+                Tab 4 with a very long title to test overflow
+            </a>
+        </div>
+        <div id="tab1vertical" data-tabpanel>
+            <p>1 - Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            <br>
+            <b><a href="#">Use the <kbd>tab</kbd> key to access this link inside the tab content</a></b>.
+            <br>
+            Voluptatum maxime amet omnis numquam voluptatem exercitationem nemo deleniti dolorem perferendis vitae? Qui molestias inventore illum ad ullam eum unde omnis temporibus.</p>
+        </div>
+        <div id="tab2vertical" data-tabpanel>
+            <p>2 - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum maxime amet omnis numquam voluptatem exercitationem nemo deleniti dolorem perferendis vitae? Qui molestias inventore illum ad ullam eum unde omnis temporibus.</p>
+        </div>
+        <div id="tab3vertical" data-tabpanel>
+            <p>3 - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum maxime amet omnis numquam voluptatem exercitationem nemo deleniti dolorem perferendis vitae? Qui molestias inventore illum ad ullam eum unde omnis temporibus.</p>
+        </div>
+        <div id="tab4vertical" data-tabpanel>
+            <p>4 - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum maxime amet omnis numquam voluptatem exercitationem nemo deleniti dolorem perferendis vitae? Qui molestias inventore illum ad ullam eum unde omnis temporibus.</p>
+        </div>
+    </webui-tabs>
+</div>
 `;
